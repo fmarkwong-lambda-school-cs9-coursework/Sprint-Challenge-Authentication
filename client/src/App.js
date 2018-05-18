4975a1dc6e0e0bb4418416bc3423f4f1a4fe6d40
Mark Wong <fmarkwong@gmail.com>
2018-05-18 14:08:19 -0700

    client setup, login form

---
+++ client/src/App.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import './App.css';
+
+import Login from './components/login';
+
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div class="container">
+        <Login />
+      </div>
+    );
+  }
+}
+
+export default App;
